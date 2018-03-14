b0874849a967f99a2508a0950a8d7a07a18d5d2f
trymsk <trymsk@users.noreply.github.com>
2018-03-14 11:08:00 +0100

    fix

--- main/main.js
+++ main/main.js
@@ -7,7 +7,7 @@ let mainWindow;
 app.on('ready', () => {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/login.html');
 });
 
 
