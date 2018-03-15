e578cc84643d273e9c6bfb8c405a7c9865a5e44f
trymsk <trymsk@users.noreply.github.com>
2018-03-15 14:23:43 +0100

    Registreringsskjema og kalender

--- main/src/app.js
+++ main/src/app.js
@@ -1,6 +1,7 @@
+
 const mysql = require('mysql');
 
-// Setup MySQL-server connection
+// MySQL kobling
 const connection = mysql.createConnection({
   host     : 'mysql.stud.iie.ntnu.no',
   user     : 'g_oops_10',  // Replace [username] with your username
@@ -8,19 +9,7 @@ const connection = mysql.createConnection({
   database : 'g_oops_10'   // Replace [username] with your username
 });
 
-// Kobling til MySQL-server
+
 connection.connect(function(error) {
   if(error) throw error; // If error, show error in console and return from this function
 });
-
-var uname = document.getElementById('uname');
-var password = document.getElementById('psw');
-
-// Funksjon for å logge inn
-function logIn(){
-  if(uname === "trymskogseth@hotmail.com" && password === "1q2w3e"){
-  mainWindow.loadURL('fille://' + __dirname + '/main_site.html')
-} else {
-  window.alert("Feil brukernavn eller passord");
-}
-};
